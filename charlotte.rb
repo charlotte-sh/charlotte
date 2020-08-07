cc3fb3e4cc90afa9e80263c869dbd5a738dc5b7e
Jura <hello@mynameisjura.com>
2020-08-07 23:34:47 +0300

    add prompt

--- charlotte.rb
+++ charlotte.rb
@@ -1,3 +1,5 @@
+require 'readline'
+
 puts
 puts '   ________               __      __  __.    v0.0 '
 puts '  / ____/ /_  ____ ______/ /___  / /_/ /____      '
@@ -5,3 +7,9 @@ puts ' / /   / __ \/ __ `/ ___/ / __ \/ __/ __/ _ \     '
 puts '/ /___/ / / / /_/ / /  / / /_/ / /_/ /_/  __/     '
 puts '\____/_/ /_/\__,_/_/  /_/\____/\__/\__/\___/      '
 puts
+
+prompt = '> '
+
+while input = Readline.readline(prompt, true)
+  puts input
+end
